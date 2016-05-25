eb459470e6be866f34ac28731d037f83dafb7566
TJSteveMX <ec.lento@gmail.com>
2016-05-25 07:52:24 -0700

    Update and rename tesla.mk to validus.mk

--- validus.mk
+++ validus.mk
@@ -2,15 +2,15 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-$(call inherit-product, vendor/tesla/config/common.mk)
+$(call inherit-product, vendor/validus/config/common.mk)
 
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := tesla_bullhead
+PRODUCT_NAME := validus_bullhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5x
 TARGET_MANUFACTURER := LGE
@@ -18,5 +18,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MMB29Q/2480792:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MMB29Q 2480792 release-keys"
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC19T/2741993:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC19T 2741993 release-keys"
