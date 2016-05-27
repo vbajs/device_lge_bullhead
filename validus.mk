6f3b27778799d69606086cc9e1ea64e50a0a4518
TJSteveMX <ec.lento@gmail.com>
2016-05-26 21:29:53 -0700

    Update validus.mk

--- validus.mk
+++ validus.mk
@@ -2,9 +2,11 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-$(call inherit-product, vendor/validus/config/common.mk)
-
+# Inherit some common Validus stuff.
 $(call inherit-product, vendor/validus/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
@@ -12,11 +14,12 @@ $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := validus_bullhead
 PRODUCT_BRAND := google
-PRODUCT_MODEL := Nexus 5x
+PRODUCT_MODEL := Nexus 5X
 TARGET_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
     BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC19T/2741993:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC19T 2741993 release-keys"
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC19T 2741993 release-keys" \
+    BUILD_ID=MTC19T
